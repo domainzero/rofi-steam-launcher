b0ad9a869c99ea8a7d6eaaf3d3fb4b7bf6c74575
domainzero <admin@mythicpl.us>
2020-08-17 14:47:36 -0500

    big dummy mistake

--- generate-menu.sh
+++ generate-menu.sh
@@ -10,21 +10,25 @@ build_csv () {
 }
 
 # Build out the first part of the menu file - this is static
-echo -e "#!/bin/bash\n\ntitle='Games Launcher'\nwidthpercent=100%\n\ntypeset -A menu\nmenu=(" > $OUTPUT_LOC
+if test -f "$OUTPUT_LOC"; then
+	echo "$OUTPUT_LOC" already exists. Rebuilding games list.
+	sed -i '10,1000d' $OUTPUT_LOC 
+	while IFS=, read -r field1 field2; do
+		echo -e "\t[$field2]='/usr/bin/steam steam://rungameid/$field1'"
+	done <<< "$(build_csv)" >> $OUTPUT_LOC
+	echo -e ")" >> $OUTPUT_LOC
+else
+	echo "$OUTPUT_LOC" not present. Building whole file.
+	echo -e "#!/bin/bash\n\ntitle='Games Launcher'\nwidthpercent=100%\n\ntypeset -A menu\nmenu=(" > $OUTPUT_LOC
+	while IFS=, read -r field1 field2; do
+		echo -e "\t[$field2]='/usr/bin/steam steam://rungameid/$field1'"
+	done <<< "$(build_csv)" >> $OUTPUT_LOC
+	echo -e ")" >> $OUTPUT_LOC
+fi
 
 # wipe out any existing games in the game menu already
 # this starts at line 10 for me because I have a few games I launch via Lutris in addition to Steam
-
-sed -i '10,1000d' $OUTPUT_LOC 
-
 # read in the comma seperated values, then spit each game in a formatted line
-
-while IFS=, read -r field1 field2; do
-    echo -e "\t[$field2]='/usr/bin/steam steam://rungameid/$field1'"
-done <<< "$(build_csv)" >> $OUTPUT_LOC
-
 # stick in the closing parentheses
-echo -e ")" >> $OUTPUT_LOC
-
 # done!
 echo "Now launch via rofigen! rofigen ~/.local/bin/game-menu"
